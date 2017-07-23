3714446cf62073b950916a2f9e5477de7ea25bf7
ledfox <atomicmatt3@gmail.com>
2017-07-23 17:40:41 -0400

    Add files via upload

--- Armor.java
+++ Armor.java
@@ -1,5 +1,25 @@
 package primary;
 
 public class Armor {
+	
+	String[] type = {"Leather","Studded Leather","Brigandine","Splintmail","Platemail"};
 
+	int typeID = 0;
+	
+	String currentType = type[typeID];
+	
+	public void upgrade(){
+
+		if(typeID < 4){
+		typeID += 1;
+		currentType = type[typeID];
+	} 
+}
+	//Constructor
+    
+    public Armor (){
+    	currentType = type[typeID];
+    }
+	
+	
 }
