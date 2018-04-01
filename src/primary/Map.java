8432889708ffa4ac24b03070aabb3e3511ea8ee5
ledfox <atomicmatt3@gmail.com>
2018-04-01 13:19:04 -0400

    Altars now change class

---
+++ src/primary/Map.java
@@ -0,0 +1,69 @@
+package primary;
+
+import net.slashie.libjcsi.CSIColor;
+import net.slashie.libjcsi.ConsoleSystemInterface;
+
+public class Map {
+
+	
+	//Methods
+	public static void drawMap(ConsoleSystemInterface mcsi){
+		
+		//Hardcoded basic map
+				mcsi.print(1,  2, "##############################################################################", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  3, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  4, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  5, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  6, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  7, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  8, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1,  9, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 10, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 11, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 12, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 13, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 14, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 15, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 16, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 17, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 18, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 19, "#............................................................................#", ConsoleSystemInterface.GRAY);
+				mcsi.print(1, 20, "##############################################################################", ConsoleSystemInterface.GRAY);
+		
+	}
+	
+	public static void setUp(ConsoleSystemInterface mcsi, String statmes, Player PC, String timeStr){
+		//Displays statmes
+		mcsi.print(1, 21, statmes, CSIColor.BABY_BLUE );
+		
+		//Displays player statistics
+				mcsi.print(1, 22, "Strength " + PC.strength + "   " 
+						+ "Vision " + PC.vision + "   " 
+						+ "Health " + PC.currentHealth + "/" + PC.maxHealth + "   " 
+						+ "Experience " + PC.experience + "   "
+						+ "Turns " + timeStr, CSIColor.BABY_BLUE);
+		
+		//Display equipment
+		mcsi.print(1, 23, "Melee: " + PC.weapon.currentType + "   "
+						+ "Ranged: " + PC.bow.currentType + "   " 
+						+ "Armor: " + PC.armor.currentType);
+		
+		//Display class
+		mcsi.print(1, 24, "Class: " + PC.currentClass);
+		
+		if(PC.currentClass.contentEquals("Rogue")){
+			
+		}
+		
+		
+		
+		//Display consumables
+		//mcsi.print(1, 24, "Ingots: " + PC.ingots + "   "
+			//			+ "Arrows: " + PC.arrows + "   "
+				//		+ "Berries: " + PC.berries);
+	
+		
+		
+	}
+
+}
--- src/primary/Player.java
+++ src/primary/Player.java
@@ -2,6 +2,8 @@ package primary;
 
 import java.util.Random;
 
+import net.slashie.libjcsi.ConsoleSystemInterface;
+
 public class Player {
 
 	 //Tied to core gameplay: csi.print(xPosthis, yPosthis, "@", CSIColor.ATOMIC_TANGERINE);
@@ -14,74 +16,153 @@ public class Player {
 	public int maxHealth;
 	
 	public String direction = "None";
+	public String currentClass = "Rogue";
+	public boolean converting = false;
 	
-	String message = "Welcome.";
-    
 	public int currentHealth;
+	
+	public Announcer GM;
 
+	public int experience = 0;
+	
+	public Armor armor = new Armor();
+	public Weapon weapon = new Weapon();
+	public Bow bow = new Bow();
+	
+	public int ingots = 0;
+	public int arrows = 0;
+	public int berries = 10;
+	
+	public ConsoleSystemInterface csi;
+	
 //Player Methods
     
-    //Object Movement
-    
-    void pushBoulderNorth(Boulder rock){
-    	rock.moveNorth();
-    }
-    
+	//Prayer
+	
+	void pray(Altar proximate){
+		if (currentClass.equals("Rogue")){
+		GM.setMessage("This is " + proximate.name +". Would you like to convert? (y/n)");
+		this.converting = true;
+		} else {
+			GM.setMessage("You're already a dedicated " + currentClass + "!");
+		}
+	}
+	
+	
+	void convert(Altar proximate){
+		String newClass = proximate.profession;
+		setClass(newClass);
+		//currentClass = (proximate.profession);
+		GM.setMessage("You've become a " + currentClass);
+		this.converting = false;
+	}
+	
+	public boolean confirm(char confirmChar) {
+		if ((confirmChar == 'y')||(confirmChar =='Y')){
+			return true;
+		} else if ((confirmChar == 'n')||(confirmChar =='N')) {
+			return false;
+		} else {
+			this.loiter();
+			return false;
+		}
+		
+	}
+
     
     //Player Movement
 
     void moveSouth() {
     	if (this.yPos < 19){
-		this.yPos++; message ="You head south."; this.direction = "South";}
-		else {message = "You can't go there!";}
+		this.yPos++; GM.setMessage("You head south."); this.direction = "South";}
+		else {GM.setMessage("You can't go there!");}
     }
     
     void moveNorth() {
-    	if (this.yPos > 3){message = "You head north."; this.direction = "North";
+    	if (this.yPos > 3){GM.setMessage("You head north."); this.direction = "North";
     	this.yPos--;}
-    	else {message = "You can't go there!";}
+    	else {GM.setMessage("You can't go there!");
+    	}
     }
     
     void moveWest() {
-    	if (this.xPos > 2){message ="You head west."; this.direction = "West";
+    	if (this.xPos > 2){GM.setMessage("You head west."); this.direction = "West";
 		this.xPos--;}
-		else {message = "You can't go there!";}
+		else {GM.setMessage("You can't go there!");
+		}
     }
     
     void moveEast() {
-    	if (this.xPos < 77) {message ="You head east."; this.direction = "East";
+    	if (this.xPos < 77) {GM.setMessage("You head east."); this.direction = "East";
 		this.xPos++;}
-		else {message = "You can't go there!";}
+		else {GM.setMessage("You can't go there!");
+		}
     }
     
     void moveNorthWest(){
-		if (this.yPos > 3 && this.xPos > 2){message ="You head northwest.";
+		if (this.yPos > 3 && this.xPos > 2){GM.setMessage("You head northwest.");
 		this.yPos--; this.xPos--; this.direction = "NorthWest";}
-		else{message = "You can't go there!";}
+		else{GM.setMessage("You can't go there!");
+		}
     }
 		
 	void moveNorthEast(){
-		if (this.yPos > 3 && this.xPos < 77){message ="You head northeast.";
+		if (this.yPos > 3 && this.xPos < 77){GM.setMessage("You head northeast.");
 		this.yPos--; this.xPos++; this.direction="NorthEast" ;}
-		else{message = "You can't go there!";}
+		else{GM.setMessage("You can't go there!");
+		}
 	}
 		
 	void moveSouthWest(){
-		if (this.yPos < 19 && this.xPos > 2){message ="You head southwest.";
+		if (this.yPos < 19 && this.xPos > 2){GM.setMessage("You head southwest.");
 		this.yPos++; this.xPos--; this.direction="SouthWest" ;}
-		else{message = "You can't go there!";}
+		else{GM.setMessage("You can't go there!");
+		}
 	}
 		
 	void moveSouthEast(){
-		if (this.yPos < 19 && this.xPos < 77){message ="You head southeast.";
+		if (this.yPos < 19 && this.xPos < 77){GM.setMessage("You head southeast.");
 		this.yPos++; this.xPos++;  this.direction="SouthEast";}
-		else{message = "You can't go there!";}
+		else{GM.setMessage("You can't go there!");
+		}
 	}
 	
 	void loiter(){
-		message = "You loiter about.";
+		GM.setMessage("You loiter about.");
 	}
 
+//Gather
+	
+	void gainBerry(){
+		this.berries += 1;
+	}
+	
+	void gainIngot(){
+		this.ingots += 1;
+	}
+	
+	void gainArrow(){
+		this.arrows += 1;
+	}
+
+//Consume
+	
+	void eatBerry(){
+		if (this.berries <= 0){
+			GM.setMessage("You're all out of berries!");
+		} else if (currentHealth == maxHealth){
+			GM.setMessage("You're too full to eat any berries!");
+		} else {
+			currentHealth += 3;
+			if (currentHealth > maxHealth){
+				currentHealth = maxHealth;
+			}
+			berries -= 1;
+			GM.setMessage("The berry was delicious!");
+		}
+	}
+	
+//Stat stuff	
 	
 void rollStat (String desStat){
 	Random rand = new Random();
@@ -107,6 +188,110 @@ void rollStat (String desStat){
 				
 }
 
+void trainStat (String desStat){
+	Random rand = new Random();
+	int d100 = rand.nextInt(100) + 1;
+	//int d20 = rand.nextInt(20) + 1;
+	int d50 = rand.nextInt(50) + 1;
+	
+	switch(desStat){
+	case "Strength":
+	if ((d100 - this.strength) > 75){
+		this.strength += 1;
+		GM.setMessage("You feel stronger!");
+	} break;
+		
+		
+	case "Vision":
+		if ((d50 - this.vision) > 5){
+			this.vision += 1;
+			GM.setMessage("You feel like a visionary!");
+		}
+	break;
+		
+	case "Health":
+		if ((d100 - this.maxHealth) > 50){
+			this.maxHealth += 5;
+			GM.setMessage("You feel more robust!");
+		}
+		
+	break;
+	
+	}
+}
+
+
+void gainXP(Actor mob){
+	
+	this.experience += mob.experience;
+	
+}
+
+void gainXP(int gain){
+	this.experience += gain;
+}
+
+void checkXP(){
+	if (this.experience >= 100){
+		gainLevel();
+	} 
+}
+
+void gainLevel(){
+	trainStat("Strength");
+	trainStat("Vision");
+	trainStat("Health");
+	this.maxHealth += 3;
+	this.experience = 0;
+	setHP(maxHealth);
+	GM.setMessage("You've gained a level! You feel fantastic!");
+}
+
+//Attack
+void meleeAttack(Actor mob){
+	Random rand = new Random();
+	int D20 = rand.nextInt(20) + 1;
+	int luckFactor = D20 + vision/2;
+	int harm;
+	if (strength/5 > 1){
+		harm = (strength/5) + weapon.quality;
+	} else harm = 1 + weapon.quality;
+	
+	if ((xPos == mob.xPos) && (yPos == mob.yPos) && (luckFactor > 10)){
+		mob.damage(harm);
+		Actor.pushBack(this);
+		GM.setMessage("You hit the " + mob.name + "!");
+	} else if ((xPos == mob.xPos) && (yPos == mob.yPos) && (luckFactor < 10)){
+		Actor.pushBack(this);
+		GM.setMessage("You miss the " + mob.name + ".");
+	}
+}
+
+void rangedAttack(Actor mob){
+	Random rand = new Random();
+	int D20 = rand.nextInt(20) + 1;
+	int luckFactor = D20 + vision;
+	int harm;
+	if (strength/5 > 1){
+		harm = (strength/5) + bow.quality;
+	} else harm = 1 + bow.quality;
+	if (luckFactor > 10){
+	mob.damage(harm);
+	GM.setMessage("Your arrow strikes the " + mob.name + "!");
+	} else {
+		GM.setMessage("Your arrow misses the " + mob.name + ".");
+	}
+	
+	
+}
+
+//Talk to Announcer
+
+void setMessage(Announcer GM, String newMess){
+	GM.setMessage(newMess);
+	
+}
+
 //Damage 
 
 void damage(int harm){
@@ -120,22 +305,31 @@ void setHP(int value){
 
 //Constructor Methods	
 	
-  public Player(int a, int b) {
+  public Player(int a, int b, Announcer vGM, ConsoleSystemInterface Pcsi) {
   	xPos =  a;
   	yPos =  b;
-  	
+  	GM = vGM;
+  	csi = Pcsi;
   }
 
-//Getter
+//Getters
   
-String getMessage(){
-	return message;
-}
+  public void getPosition(){
+	  System.out.println("xPos " + xPos);
+	  System.out.println("yPos " + yPos);
+	  System.out.println("");
+  }
+  
+  public void getCharClass(){
+	  System.out.print(this.currentClass);
+  }
   
 //Setter
+ 
 
-void setMessage(String newMess){
-	message = newMess;
+public void setClass(String newClass) {
+	// TODO Auto-generated method stub
+	currentClass = newClass;
 }
 
 
